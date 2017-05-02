b0e0dccf908bcbdfb9d0541d01269fb364d60092
kshitizsekhri <kshitiz.sekhri@twinspark.co>
2017-05-02 13:48:49 +0530

    Resolve version issues

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -38,7 +38,7 @@ class PagesController extends AppController
      * @throws \Cake\Network\Exception\NotFoundException When the view file could not
      *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
      */
-    public function display(...$path)
+    public function display($path)
     {
         $count = count($path);
         if (!$count) {
